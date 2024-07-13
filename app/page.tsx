c715a0b3a727e7569683537d5469734b9dd2c999
mathis sergent <sergent.mathis@gmail.com>
2024-07-13 15:17:16 +0200

    Tmp Safe data not used for the moment

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,15 @@ import { Button } from "@/components/ui/button";
 import { useSafeAppsSDK } from "@safe-global/safe-apps-react-sdk";
 
 export default function Home() {
-  const { sdk, connected, safe } = useSafeAppsSDK();
+  // const { sdk, connected, safe } = useSafeAppsSDK();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p>Hello Safe: {safe.safeAddress}</p>
-
-        <button
-          onClick={async () => {
-            const response = await sdk.safe.requestAddressBook();
-            console.log("Safe call response:", response);
-          }}
-        >
-          Safe trigger
-        </button>
+        {/* <p>Hello Safe: {safe.safeAddress}</p> */}
 
         <Link href="/dashboard">
-          <Button>Launch App</Button>
+          <Button>Connect wallet</Button>
         </Link>
       </div>
     </main>
