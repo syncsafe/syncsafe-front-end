22aa00cb5ee2c6df9060080e4cf431cb851f265b
Baptiste Florentin <baptiste@nefture.com>
2024-07-25 09:39:37 +0200

    new prod mainnet indexer url

--- services/graphql/graphql.ts
+++ services/graphql/graphql.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 // Initialize the Apollo Client
 export const client = new ApolloClient({
-  uri: "https://mainnet-indexer-production.up.railway.app/",
+  uri: "https://indexer-production-b662.up.railway.app/",
   cache: new InMemoryCache(),
 });
